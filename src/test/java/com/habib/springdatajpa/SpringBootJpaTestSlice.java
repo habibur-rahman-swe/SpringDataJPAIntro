daf95c8f8c996e8535b6f6f7dfbfb4bf62bdee10
Habibur Rahman <habibur0199mhr@gmail.com>
2024-05-10 12:45:59 +0600

    Test data transaction

--- src/test/java/com/habib/springdatajpa/SpringBootJpaTestSlice.java
+++ src/test/java/com/habib/springdatajpa/SpringBootJpaTestSlice.java
@@ -2,21 +2,31 @@ package com.habib.springdatajpa;
 
 import com.habib.springdatajpa.domain.Book;
 import com.habib.springdatajpa.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+//    @Rollback(value = false)
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book("A", "B", "C"));
 
@@ -26,4 +36,10 @@ public class SpringBootJpaTestSlice {
 
     }
 
+    @Order(2)
+    @Test
+    void testJpaTestJpaTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
+    }
 }
